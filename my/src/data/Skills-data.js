6de658941d9759f2debf7f04a7360d68ae3bf3d1
CherifMoh <mircho.dev@gmail.com>
2024-05-28 00:09:03 +0100

    add skills

--- my/src/data/Skills-data.js
+++ my/src/data/Skills-data.js
@@ -4,6 +4,11 @@ import css from "../assets/css3.svg";
 import js from "../assets/javascript.svg";
 import Wp from "../assets/wordpress.svg";
 import React from "../assets/react.svg";
+import nodejs from "../assets/nodejs.png";
+import mongodb from "../assets/mongodb.svg";
+import nextjs from "../assets/nextjs.svg";
+import github from "../assets/github.svg";
+import git from "../assets/Git.svg";
 
 
 export const skills =[
@@ -26,6 +31,26 @@ export const skills =[
     },{
       skillImg:React,
       skillName:'React',
+      id:uuid(),
+    },{
+      skillImg:nodejs,
+      skillName:'node js',
+      id:uuid(),
+    },{
+      skillImg:mongodb,
+      skillName:'mongodb',
+      id:uuid(),
+    },{
+      skillImg:nextjs,
+      skillName:'next js',
+      id:uuid(),
+    },{
+      skillImg:github,
+      skillName:'github',
+      id:uuid(),
+    },{
+      skillImg:git,
+      skillName:'git',
       id:uuid(),
     }
 ]
